41549f2e41aad669b240300093b5d660efd7003a
Thomas Kluyver <thomas@kluyver.me.uk>
2016-03-28 11:09:09 -0700

    Tab completion

--- ubit_kernel/kernel.py
+++ ubit_kernel/kernel.py
@@ -1,3 +1,5 @@
+import ast
+import re
 import time
 
 from ipykernel.kernelbase import Kernel
@@ -30,9 +32,8 @@ class MicrobitKernel(Kernel):
         super().__init__(**kwargs)
         self.serial = connect()
 
-    def do_execute(self, code, silent, store_history=True,
-                   user_expressions=None, allow_stdin=False):
-
+    def run_code(self, code):
+        '''Run a string of code, return strings for stdout and stderr'''
         self.serial.write(code.encode('utf-8') + b'\x04')
         result = bytearray()
         while not result.endswith(b'\x04>'):
@@ -43,16 +44,48 @@ class MicrobitKernel(Kernel):
 
         assert result.startswith(b'OK')
         out, err = result[2:-2].split(b'\x04', 1)
+        return out.decode('utf-8', 'replace'), err.decode('utf-8', 'replace')
+
+    def do_execute(self, code, silent, store_history=True,
+                   user_expressions=None, allow_stdin=False):
+        out, err = self.run_code(code)
         if out:
             self.send_response(self.iopub_socket, 'stream', {
                 'name': 'stdout',
-                'text': out.decode('utf-8', 'replace')
+                'text': out
             })
         if err:
             self.send_response(self.iopub_socket, 'stream', {
                 'name': 'stderr',
-                'text': err.decode('utf-8', 'replace')
+                'text': err
             })
 
         return {'status': 'ok', 'execution_count': self.execution_count,
                 'payload': [], 'user_expressions': {}}
+
+    def _eval(self, expr):
+        out, err = self.run_code('print({})'.format(expr))
+        return ast.literal_eval(out)
+
+    def do_complete(self, code, cursor_pos):
+        import sys
+        print('completing on', repr(code), file=sys.__stderr__)
+        code = code[:cursor_pos]
+        m = re.search(r'(\w+\.)*(\w+)?$', code)
+        if m:
+            prefix = m.group()
+            print('prefix', repr(prefix), file=sys.__stderr__)
+            if '.' in prefix:
+                obj, prefix = prefix.rsplit('.')
+                names = self._eval('dir({})'.format(obj))
+            else:
+                names = self._eval('dir()')
+            print('names', names, file=sys.__stderr__)
+            matches = [n for n in names if n.startswith(prefix)]
+            return {'matches': matches,
+                    'cursor_start': cursor_pos - len(prefix), 'cursor_end': cursor_pos,
+                    'metadata': {}, 'status': 'ok'}
+        else:
+            return {'matches': [],
+                    'cursor_start': cursor_pos, 'cursor_end': cursor_pos,
+                    'metadata': {}, 'status': 'ok'}
